f90c924f7f48587ed8a52fcd6b16b7d7c8d9bdb4
Julien-MANSOUR <j8mansou@enib.fr>
2020-12-16 22:27:19 +0100

    Setting up the project and preparing the main window

---
+++ MyEmployees.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My Employees")
+        self.setGeometry(450, 150, 750, 600)
+        self.UI()
+        self.show()
+
+    def UI(self):
+        self.mainDesign()
+
+    def mainDesign(self):
+        pass
+
+
+def main():
+    App = QApplication(sys.argv)
+    window = Main()
+    sys.exit(App.exec_())
+
+
+if __name__ == '__main__':
+    main()
